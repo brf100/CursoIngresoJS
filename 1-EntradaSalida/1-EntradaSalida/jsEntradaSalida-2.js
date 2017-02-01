0cf86f4f2754ea26c2772578475a64c31a484c3b
brf100 <brf_100@hotmail.com>
2017-02-01 12:00:14 -0300

    Add files via upload

---
+++ 1-EntradaSalida/1-EntradaSalida/jsEntradaSalida-2.js
@@ -0,0 +1,20 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+//comentario
+//linea w3c.com (window prompt method)
+{
+	var nombre;//defino variable
+    nombre="Brian";//cargo variable
+
+nombre=prompt("ingrese su nombre","natalia natalia")// prompt:devolver algo
+
+
+	alert ("nombre:  "+nombre);//concatenar: tomar valores literales con valores de variables
+
+
+
+
+}
+
+

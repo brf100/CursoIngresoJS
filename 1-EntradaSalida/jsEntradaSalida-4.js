16a0cbdc7a46f6caf2ceb356c34a0a75d1e8c54b
brf100 <brf_100@hotmail.com>
2017-02-07 12:13:31 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -3,7 +3,8 @@
 	y lo muestro por 'getElementById' al presionar el botón 'MOSTRAR'
 */
 function Mostar()
-{
+{ prompt("nombre");
+	document getElementById("nombre"); 
 	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,11 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
 {	
-	
+ 
+var("nombre");
+
+nombre=document.getElementById("nombre");
+
+
 }
 
